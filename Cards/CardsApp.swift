2a382e5b5471cc8e97d753a1390c3e8986272c4f
Manukant Tyagi <mactyagi@icloud.com>
2024-09-06 08:22:03 +0530

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Manu on 06/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
